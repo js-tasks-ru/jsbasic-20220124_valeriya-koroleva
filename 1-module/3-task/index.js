0dd2601a57dd96b64d0bc12ea4ead5b42dfdc836
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-02-03 15:52:31 +0300

    add solution first-symbol

---
+++ 1-module/3-task/index.js
@@ -0,0 +1,10 @@
+function ucFirst (str) {
+  if (!str) {
+    return str;
+  } else {
+      let strAsArr = str.split('');
+      strAsArr[0] = strAsArr[0].toUpperCase();
+      return strAsArr.join('');
+  }
+}
+let firstname = ucFirst('вася');
