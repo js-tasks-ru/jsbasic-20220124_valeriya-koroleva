542dbc10897d712cb5d0d107459d3cfbc7a473b7
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-02-03 16:47:51 +0300

    add solution spam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+   let patterns = ['1xBet', 'XXX'];
+
+   for (const pattern of patterns) {
+    if (str.toLowerCase().indexOf(pattern.toLowerCase()) > -1) {
+      return true;
+    }
+   }
+   return false;
 }
+let input = checkSpam('innocent rabbit');
