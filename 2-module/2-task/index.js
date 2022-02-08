3952d5e675c66966399202d7fa8baf5f3e09e716
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-02-08 15:28:02 +0300

    add is Empty

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,12 @@
 function isEmpty(obj) {
-  // ваш код...
+ 
+  return !Object.keys(obj).length
 }
+
+let schedule = {};
+
+alert( isEmpty(schedule) );
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) );
