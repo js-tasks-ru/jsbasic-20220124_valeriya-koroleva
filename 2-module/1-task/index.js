e5ca8189c38805cc631bbd3442bcb165090b7e6f
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-02-22 20:47:04 +0300

    add new decision

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -2,8 +2,9 @@ function sumSalary(salaries) {
   let sum = 0;
    for (let salary of Object.values(salaries)) {
 
-          if(typeof salary === 'number' && isFinite(salary) &&
-              !(isNaN(salary))) {
+          if(typeof salary === 'number' &&
+              !(isNaN(salary)) && (isFinite(salary))) {
+
            sum+= salary;
       }
   }
