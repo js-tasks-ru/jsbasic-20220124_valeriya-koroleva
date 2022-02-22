53af1885109a58146ccc3e786a9c721be507770b
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-02-22 20:31:45 +0300

    add new decision

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -3,3 +3,11 @@ function isEmpty(obj) {
     return false;
   } return true;
 }
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) ); // false
