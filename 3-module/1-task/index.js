2033492a4732b485ec234a14b9d07ff8abae22f6
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-02-22 22:27:55 +0300

    add decision

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,9 @@
 function namify(users) {
-  user.map(item => user.name);
+ let arrName = [];
+ for (let i = 0; i < users.length; i++) {
+    let user = users[i];
+    let name = user.name;
+    arrName.push(name);
+ }
+ return arrName;
 }
