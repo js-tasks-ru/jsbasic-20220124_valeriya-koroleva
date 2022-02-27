d3b7ec17865581d0a0253afbf07ca46e95fcb69b
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-02-27 21:22:08 +0300

    add

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,10 @@
+// let table = document.querySelector('table');
+let rows = table.rows;
+
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let i = 0; i < rows.length; i++) {
+    rows[i].cells[i].style.background = 'red';
+  }
 }
+
+// makeDiagonalRed(table);
