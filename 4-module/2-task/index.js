4cfd07fe94ee29d1641f64793d6a470dbf984cc6
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-02-27 21:35:34 +0300

    add new makeDiagonalRed

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,5 +1,10 @@
+let table = document.querySelector('table');
+let rows = table.rows;
+
 function makeDiagonalRed(table) {
   for (let i = 0; i < rows.length; i++) {
     rows[i].cells[i].style.background = 'red';
   }
 }
+
+makeDiagonalRed(table);
