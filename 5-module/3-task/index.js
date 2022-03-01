07d9f82c510f73bb756da77af3993037f626e9d9
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-03-01 16:56:42 +0300

    add initCarousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -2,42 +2,36 @@ function initCarousel() {
   let btnNext = document.querySelector('.carousel__arrow_right');
   let btnPrev = document.querySelector('.carousel__arrow_left');
   let sliger = document.querySelector('.carousel__inner');
-  let viewSliders = document.querySelectorAll('.carousel__slide');
   let viewport = document.querySelector('.container').offsetWidth;
   let viewSlide = 0;
   
-
 btnNext.addEventListener('click', goNextCarosel);
 btnPrev.addEventListener('click', goPrevCarosel);
-
-noneBtn();
 
   function goNextCarosel() {
     if (viewSlide < 3) {
         viewSlide++;
-    } else {
-        viewSlide = 0;
     }
     let position = -viewSlide * viewport + "px";
     sliger.style.transform = `translateX(${position})`;
+
+    if (viewSlide == 3) {
+      btnNext.style.display = 'none';
+    } else {
+      btnPrev.style.display = '';
+    }
   }
 
   function goPrevCarosel () {
     if (viewSlide > 0) {
       viewSlide--;
-  } else {
-      viewSlide = 3;
-  }
+  } 
   let position = -viewSlide * viewport + "px";
   sliger.style.transform = `translateX(${position})`;
+  if (viewSlide == 0) {
+    btnPrev.style.display = 'none';
+  } else {
+    btnNext.style.display = '';
   }
-  function noneBtn () {
-    if (viewSlide === 0) {
-      btnPrev.style.display = 'none';
-      btnNext.style.display = '';
-    } else if (viewSlide === 3) {
-      btnNext.style.display = 'none';
-      btnPrev.style.display = '';
-    }
   }
 }
