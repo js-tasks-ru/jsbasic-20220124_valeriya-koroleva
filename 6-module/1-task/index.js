ecf355bf478d77cd34056ef62a3413644059ac20
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-03-02 23:38:56 +0300

    add UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -4,31 +4,33 @@ export default class UserTable {
     this.tbody = document.createElement('tbody');
     this.rows = rows;
 
+    this.table.innerHTML = `<thead>
+    <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+    </tr>
+  </thead>`;
+  
+  this.table.appendChild(this.tbody);
+
+  for (const row of this.rows) {
+    this.tbody.insertAdjacentHTML('beforeEnd', 
+    `<tr>
+    <td>${row.name}</td>
+    <td>${row.age}</td>
+    <td>${row.salary}</td>
+    <td>${row.city}</td>
+    <td><button>X</button></td>
+    </tr>`);
+  }
+
+  this.table.addEventListener('click', this.onClick);
   }
 
   get elem() {
-      this.table.innerHTML = `<thead>
-      <tr>
-        <th>Имя</th>
-        <th>Возраст</th>
-        <th>Зарплата</th>
-        <th>Город</th>
-        <th></th>
-      </tr>
-    </thead>`;
-    this.table.appendChild(this.tbody);
-    for (const row of this.rows) {
-      this.tbody.insertAdjacentHTML('beforeEnd', 
-      `<tr>
-      <td>${row.name}</td>
-      <td>${row.age}</td>
-      <td>${row.salary}</td>
-      <td>${row.city}</td>
-      <td><button>X</button></td>
-      </tr>`);
-    }
-
-    this.table.addEventListener('click', this.onClick);
 
     return this.table;
   }
