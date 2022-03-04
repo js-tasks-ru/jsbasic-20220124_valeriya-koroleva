07cfc55956c8924e32b7ac799f45c3b1dea003fd
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-03-04 21:56:53 +0300

    add ProductCard

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,32 @@
 export default class ProductCard {
   constructor(product) {
-  }
+    this.product = product;
+    this.card = document.createElement('div');
+    this.card.classList.add("card");
+
+    this.card.insertAdjacentHTML('beforeEnd', 
+      `<div class="card__top">
+          <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+          <span class="card__price">€${product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+          <div class="card__title">${product.name}</div>
+          <button type="button" class="card__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+      </div>`);
+
+      this.card.addEventListener('click',(event) =>{
+        const btn = event.target.closest('.card__button') 
+      if(btn){
+        const productAddBtn = new CustomEvent('product-add',{
+          detail: this.product.id,
+          bubbles : true,
+        })
+        btn.dispatchEvent(productAddBtn);
+      }})
+}
+get elem() {
+  return this.card;
+}
 }
